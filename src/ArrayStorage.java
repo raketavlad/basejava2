810022f1aeb5099973920ff567a17fa952738855
raketavlad <zerosworld@mail.ru>
2026-04-02 22:15:27 +0300

    Lesson_1. Моя реализация задания

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
-/**
- * Array based storage for Resumes
- */
+import java.util.Arrays;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        int index = getResumeIndex(resume.uuid);
+        if (index >= 0) {
+            System.out.println("Резюме с таким uuid уже есть в хранилище!");
+        } else {
+            storage[size()] = resume;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = getResumeIndex(uuid);
+        if (index < 0) {
+            System.out.println("Резюме с таким uuid не найдено!");
+            return null;
+        } else {
+            return storage[index];
+        }
     }
 
     void delete(String uuid) {
+        int index = getResumeIndex(uuid);
+        if (index < 0) {
+            System.out.println("Резюме с таким uuid не найдено!");
+        } else {
+            int size = size();
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int countResume = 0;
+        while (storage[countResume] != null) {
+            countResume++;
+        }
+        return countResume;
+    }
+
+    private int getResumeIndex(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 }
